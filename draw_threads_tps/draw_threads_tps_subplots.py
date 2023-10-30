a16adf4e2b743dafa0e4b6f055746946c991e056
yeexiaozheng <yeexiaozheng@gmail.com>
2023-10-30 20:10:21 +0800

    thread tps

--- draw_threads_tps/draw_threads_tps_subplots.py
+++ draw_threads_tps/draw_threads_tps_subplots.py
@@ -15,7 +15,7 @@ sys.path.extend(['.', '..'])
 import matplotlib.pyplot as plt
 from parse import parse_meta, parse_record, parse_records_from_file
 from plot import MyPlot
-from common import adaptive_y, to_fomat
+from common import adaptive_y, to_fomat, add_serial
 
 def plot_by_protocol(
         p: MyPlot, ax: plt.Axes,
@@ -27,7 +27,7 @@ def plot_by_protocol(
     y, ylabel = y
 
     max_y = 0
-    for idx, (protocol, color) in enumerate(protocols):
+    for idx, (protocol, color, marker) in enumerate(protocols):
         p.plot(
             ax,
             records[records['protocol'] == protocol.lower()][x], 
@@ -47,6 +47,10 @@ def plot_by_protocol(
     max_y = int(max_y)
     step = adaptive_y(max_y)
 
+    ax.set_xticks(
+        range(6, 37, 6)
+    )
+
     ax.set_yticks(
         range(0, max_y, step), 
         [str(x)[:-3] if len(str(x)) > 3 else str(x) for x in range(0, max_y, step)]
@@ -61,6 +65,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     p = MyPlot(1, 3, (15, 4))
+    tag = ['(a)', '(b)', '(c)']
 
     for idx, file in enumerate([args.log_file1, args.log_file2, args.log_file3]):
         ax: plt.Axes = p.axes[idx]
@@ -74,21 +79,30 @@ if __name__ == '__main__':
 
         # 处理日志并生成一个data frame
         recs = parse_records_from_file(content)
+        
+        recs = recs[recs['threads'] <= 36].reset_index(drop=True)
+        add_serial(recs, 'threads')
+
+        print(idx, recs)
 
         plot_by_protocol(
             p, ax,
             recs, 
-            (X, XLABEL), (Y, YLABEL if idx == 0 else ""), 
+            (X, XLABEL + "\n" + tag[idx] + "  " + file.split("_")[-1].capitalize()), (Y, YLABEL if idx == 0 else ""), 
             [
                 # 里面是 (协议名称, 颜色(RGB格式)的元组)
                 # ('sparkle original' , 'tab:blue'),
                 # ('sparkle partial'  , 'orange'),
                 # ('aria fb'          , 'green'),
-                ('Sparkle Original' , 'blue'),
-                ('Sparkle Partial'  , 'r'),
-                ('Aria FB'          , 'lime'),
+                # ('Sparkle Original' , 'blue'),
+                # ('Sparkle Partial'  , 'r'),
+                # ('Aria FB'          , 'lime'),
+                ('sparkle partial'  , '#8E5344'    , None),
+                ('sparkle original' , '#ED9F54'    , None),
+                ('aria fb'          , '#45C686'    , None),
+                ('serial'           , '#B9A89B'    , None),
             ],
         )
 
-    p.legend(ax, anchor=(-0.75, 1.20))
+    p.legend(ax, anchor=(-0.75, 1.20), ncol=4)
     p.save("subplots" + ".pdf")
